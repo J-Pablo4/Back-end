5851242169afcd58c40f05e683192412cc922c85
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-08 19:21:28 -0600

    created controllers.js from publications

---
+++ API/src/publications/controllers.js
@@ -0,0 +1,11 @@
+const controller = {
+    create: (req, res) => {},
+    update: (req, res) => {},
+    list: (req, res) => {
+        res.send('endpoint de publications');
+    },
+    get: (req, res) => {},
+    delete: (req, res) => {}
+}
+
+module.exports = controller;
--- API/src/publications/routes.js
+++ API/src/publications/routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/publications', function(req, res){
-    res.send('endpoint de publicaciones');
+    res.send('endpoint de publications');
 });
 
 module.exports = router;
