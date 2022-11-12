883e919d716c6a98ea5ea17c460d15bc84ceee8a
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-12 15:02:34 -0600

    users ready

--- API/src/users/controllers.js
+++ API/src/users/controllers.js
@@ -2,6 +2,7 @@ const controller = {
     list: (req, res) => {
         res.send('endpoint de users');
     },
+
     update: (req, res) => {
         const id = req.params.id;
         const name = req.body.name;
@@ -9,8 +10,35 @@ const controller = {
         const email = req.body.email;
         const password = req.body.password;
         const rol = req.body.rol;
+        const publications = req.body.publications
 
         res.send('Se actualizo el usuario '+name+' '+surname+' con el id:'+id);
+    },
+    getOne: (req, res) => {
+        const id = req.params.id;
+        const name = req.body.name;
+        const surname = req.body.surname;
+        const email = req.body.email;
+        const password = req.body.password;
+        const rol = req.body.rol;
+        const publications = req.body.publications
+
+        res.send('Endpoint del usuario '+name+' '+surname+' con el id:'+id);
+    },
+    create: (req, res) => {
+        const name = req.body.name;
+        const surname = req.body.surname;
+        const email = req.body.email;
+        const password = req.body.password;
+        const rol = req.body.rol;
+
+        res.send('Se creo el usuario'+name+' '+surname);
+    }, 
+    log_in: (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        res.send('Endpoint de login');
     }
 }
 
--- API/src/users/routes.js
+++ API/src/users/routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const controller = require('./controllers');
 
 router.get('', controller.list);
-
 router.put('/:id', controller.update);
+router.get("/:id", controller.getOne);
+router.post('/sign_up', controller.create);
+router.post('/log_in', controller.log_in);
 
 module.exports = router;
