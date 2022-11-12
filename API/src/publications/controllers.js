11585edbb807917f920c8314a3a17edaf0d168df
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-12 17:12:23 -0600

    added more functionality to the publications endpoint

--- API/src/publications/controllers.js
+++ API/src/publications/controllers.js
@@ -2,27 +2,29 @@ const controller = {
     list: (req, res) => {
         res.send('endpoint de publications');
     },
-    update: (req, res) => {
+    delete: (req, res) => {
         const id = req.params.id;
-        const place = req.body.place;
-        const type = req.body.type;
-        const photo = req.body.photo;
-        const user = req.body.user;
-        const description = req.body.description;
-        const date = req.body.date;
+        const state = req.body.state;
 
-        res.send('Se actualizo la publicación '+id+' del usuario ' + user);
+        res.send('Se elimino la publicación con el id: '+id);
     },
-    publish: (req, res) => 
-    {
+    publish: (req, res) => {
         const place = req.body.place;
         const type = req.body.type;
         const photo = req.body.photo;
         const user = req.body.user;
         const description = req.body.description;
         const date = req.body.date;
+        const state = req.body.state;
 
         res.send('Se creo la publicación del usuario '+user);
+    },
+    comment: (req, res) =>{
+        const id = req.params.id;
+        const user = req.body.user;
+        const comment = req.body.comment;
+
+        res.send('El usuario '+user+ 'commento '+'"'+comment+'" en la publicacion con id '+id);
     }
 }
 
--- API/src/publications/routes.js
+++ API/src/publications/routes.js
@@ -3,6 +3,8 @@ const controller = require('./controllers');
 
 router.post('', controller.publish);
 router.get('', controller.list);
-router.put('/:id', controller.update);
+router.put('/:id/delete', controller.delete);
+router.put('/:id', controller.comment);
+
 
 module.exports = router;
