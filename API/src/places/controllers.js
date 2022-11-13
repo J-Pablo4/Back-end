fa42a8e10dcdd00d08f334552701234b4d631a34
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-13 15:36:24 -0600

    updated the rentals places

--- API/src/places/controllers.js
+++ API/src/places/controllers.js
@@ -3,14 +3,22 @@ const controller = {
     list: (req, res) => {
         res.send('endpoint de places');
     },
-    update: (req, res) => {
+    delete: (req, res) => {
         const id = req.params.id;
+        const state = req.body.delete;
+
+        res.send('Se borro el lugar con el id '+id);
+    },
+    create: (req, res) => {
         const country = req.body.country;
         const city = req.body.city;
         const continent = req.body.continent;
         const weather = req.body.weather;
 
-        res.send('Se actualizo el lugar '+city+' del pais '+country);
+        res.send('Se creo el lugar '+city+' del pais '+country);
+    },
+    list_by_weather: (req, res) => {
+        
     }
 }
 
--- API/src/places/routes.js
+++ API/src/places/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const { Router } = require('express');
 const controller = require('./controllers');
 
 router.get('', controller.list);
-router.put('/:id', controller.update);
+router.put('/:id', controller.delete);
+router.post('', controller.create);
 
 module.exports = router;
