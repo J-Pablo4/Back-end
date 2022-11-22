4256fdba21249429230d7b00c2a3775950be6171
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-22 15:44:20 -0600

    Installed, Imported and used multer

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const apiRoutes = require('./src/api');
+const multer = require('multer');
 
 const app = express();
 
@@ -11,6 +12,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(multer({
+    dest: 'public/uploads'
+}).single('photo'));
 app.use(apiRoutes);
 
 const uri = 'mongodb+srv://ITESO2022:fKsEI9EjOz82JkAL@cluster0.waa3jjj.mongodb.net/Top_TripDB?retryWrites=true&w=majority';
