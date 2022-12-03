2972e5b172cf3a7bbf7a9ea06443b69db129c261
Juan Pablo <pablo.lopez@iteso.mx>
2022-12-02 19:07:48 -0600

    make it better

--- API/src/middlewares/images.js
+++ API/src/middlewares/images.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/uploads');
+      cb(null, 'API/public/uploads');
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
